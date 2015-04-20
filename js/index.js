788be4184e8f972542ceff087c61dec6174a3162
yun5156 <yun5156@qq.com>
2015-04-20 20:50:20 +0800

    dd

--- js/index.js
+++ js/index.js
@@ -2,13 +2,23 @@
  * Created by jujube on 2015/4/13.
  */
 $(function () {
-    var vp=new Viwepager({
+    var vp1=new Viwepager({
         width:'78px',
         height:'78px',
-        imgs:[{src:'images/qrcode_vmall_app01.png',link:'',alt:'1234566'},{src:'images/qrcode_vmall_wechat01.jpg',link:'',alt:'dasdfasdfaf'}],
+        imgs:[{src:'images/qrcode_vmall_app01.png',link:'',alt:'扫码下载客户端'},{src:'images/qrcode_vmall_wechat01.jpg',link:'',alt:'微信关注我们'}],
         delay:10000,
         duration:100,
         overFlag:'over-flag-class'/**/
     });
-    vp.createTo($('#two-dimension'));
+    vp1.createTo($('#two-dimension'));
+
+    var vp2=new Viwepager({
+        width:'980px',
+        height:'400px',
+        imgs:[{src:'images/focus1.jpg',link:'',alt:''},{src:'images/focus2.jpg',link:'',alt:''},{src:'images/focus3.jpg',link:'',alt:''}],
+        delay:5000,
+        duration:'slow',
+        overFlag:'over-flag-class'/**/
+    });
+    vp2.createTo($('#focus-pic'));
 });
